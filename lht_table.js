d3adb2c1546fb7afa9a8d5d73ffaa0e8349cc106
zhatch5201 <zhatch164@wesst-mec.org>
2020-02-03 13:22:34 -0700

    Done

---
+++ lht_table.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Review Assignment
+
+   Author: 
+   Date:   
+
+	
+*/
+
+var thisDay = new Date('August 30, 2018');
+var tableHTML = '<table id="eventTable"><caption>Upcoming Events</caption><tr><th>Date</th><th>Price</th></tr>';
+var endDate = new Date(thisDay.getTime() + 14 * 24 * 60 * 60 * 1000);
+
+for (var i = 0; i < eventDates.length; i++) {
+	var eventDate = new Date(eventDates[i]);
+	var eventDay = eventDate.toDateString();
+	var eventTime = eventDate.toLocaleTimeString();
+	if (thisDay <= eventDate && eventDate <= endDate) {
+		tableHTML +=
+			'<tr><td>' +
+			eventDay +
+			' @ ' +
+			eventTime +
+			'</td><td>' +
+			eventDescriptions[i] +
+			'</td><td>' +
+			eventPrices[i] +
+			'</td></tr>';
+	}
+}
+
+tableHTML += '</table>';
+
+document.getElementById('eventList').innerHTML = tableHTML;
